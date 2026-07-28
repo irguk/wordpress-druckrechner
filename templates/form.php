67ded2cca17749a4e538a822f4b0ded0dee45f27
CodeWanderin <25100610+irguk@users.noreply.github.com>
2026-07-28 21:58:42 +0200

    feat(templates): Formular-Hauptstruktur und Formularfelder hinzugefügt

---
+++ templates/form.php
@@ -0,0 +1,6 @@
+<div class="container-fluid mt-5">
+    <div class="row d-flex align-items-start">
+        <?php include 'form-section.php'; ?>
+        <?php include 'preview-section.php'; ?>
+    </div>
+</div>
